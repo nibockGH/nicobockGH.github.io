be6a1cde99be5548f5db3ee7f61b3e8da303196f
Nicolas <nicobock15@gmail.com>
2024-05-30 01:19:13 -0300

    actualizacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nibockgh.github.io',
+  base: '/nicobockGH.github.io',
   integrations: [tailwind()]
 });
